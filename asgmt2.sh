8c7cdeefbd04b6a9bd3dfbab415cfaa21f90b47e
7788srinath <srinaths119@gmail.com>
2021-07-04 18:08:47 +0530

    first commit

---
+++ asgmt2.sh
@@ -0,0 +1,8 @@
+echo "enter 1st number :"
+read number1  
+echo "enter 2nd number :"
+read number2
+echo "addition of number1 nad number2 is :" `expr $number1 + $number2`
+echo "subraction of number1 nad number2 is :" `expr $number1 - $number2`
+echo "multiplication of number and and number2 :" `expr $number1 \* $number2`
+hi i am srinatha`
